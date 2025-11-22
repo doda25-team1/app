40fb6fd93b767c8c973aba52e98567108d519e4b
Deon Saji <deonsaji@Deons-MacBook-Air.local>
2025-11-21 17:16:47 -0800

    added space to tst app

--- src/main/java/frontend/Main.java
+++ src/main/java/frontend/Main.java
@@ -3,6 +3,7 @@ package frontend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class Main {
 
